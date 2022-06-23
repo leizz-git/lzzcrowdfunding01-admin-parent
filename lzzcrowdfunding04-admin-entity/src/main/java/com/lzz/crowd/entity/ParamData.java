09e81940a2f76a170a5b596098cbe494c0140dbf
leizhanzhou <2495884051@qq.com>
2022-06-23 21:02:52 +0800

    尚筹网项目实体子项目

---
+++ lzzcrowdfunding04-admin-entity/src/main/java/com/lzz/crowd/entity/ParamData.java
@@ -0,0 +1,19 @@
+package com.lzz.crowd.entity;
+
+import java.util.List;
+
+public class ParamData {
+
+    private List<Integer> array;
+
+    public ParamData() {
+    }
+
+    public List<Integer> getArray() {
+        return array;
+    }
+
+    public void setArray(List<Integer> array) {
+        this.array = array;
+    }
+}
